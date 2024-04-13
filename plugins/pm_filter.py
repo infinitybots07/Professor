5b73a79fa4c66cd67534795a7ca9622c66874d10
Infinity Botz <134354677+infinitybots07@users.noreply.github.com>
2024-04-13 08:56:13 +0530

    Syntex error

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -41,7 +41,7 @@ BUTTONS = {}
 SPELL_CHECK = {}
 ENABLE_SHORTLINK = ""
 
-@Client.on_message((filters.group & filters.text & filters.incoming)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
     await message.react(emoji=random.choice(REACTIONS))
     if message.chat.id != SUPPORT_CHAT_ID:
