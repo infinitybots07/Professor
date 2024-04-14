fdaf6bd0476377b9880eb206c106254950d136c3
Infinity Botz <134354677+infinitybots07@users.noreply.github.com>
2024-04-14 08:41:10 +0530

    Infinitybots07 patch 2 (#2)
    * Update Script.py
    
    * Update pm_filter.py
    
    * Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -56,7 +56,7 @@ async def start(client, message):
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, msg.from_user.id, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
@@ -81,7 +81,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(
-            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, msg.from_user.id, temp.B_NAME),
+            text=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )
